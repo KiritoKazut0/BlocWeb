e8d52f621bf377f8c99108287a70e0d0a56deef8
Draken-101 <leonardofavion@gmail.com>
2024-02-18 01:48:12 -0600

    Avance Molecula Articulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Registro from "./Components/Organisms/Registro"
 import RegistroDos from "./Components/Organisms/RegistroDos"
+import Articulo from "./Components/Molecules/Articulo"
+import Home from "./Components/Organisms/Home"
 function App() {
 
 
   return (
     <>
-      <RegistroDos/>
+      <Home/>
     </>
   )
 }
